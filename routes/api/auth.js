1e95297c482deae5c5fc0b7d35b61c8620d0522d
Adi Mordo <adids1221@gmail.com>
2021-07-20 00:27:32 +0300

    Finished user register,auth,login backend

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const User = require('../../modles/User');
 const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const config = require('config');
 
 // @route  GET api/auth
 // @desc   Test route
@@ -13,6 +17,55 @@ router.get('/', auth, async (req, res) => {
     } catch (err) {
         console.error(err.message);
         ers.status(500).send('Server Error');
+    }
+});
+
+// @route  POST api/auth
+// @desc   Authenticate user & get token
+// @access public
+router.post('/', [
+    check('email', 'Email is required').isEmail(),
+    check('password', 'Password is required').exists()
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    //Check if user exists
+    try {
+        let user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({ errors: [{ message: 'Invalid Credentials' }] });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ errors: [{ message: 'Invalid Credentials' }] });
+        }
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            { expiresIn: 360000 },
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+            }
+        );
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
     }
 });
 
