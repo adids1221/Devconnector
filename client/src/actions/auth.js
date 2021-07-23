0706c5963a5ddda7ea716b33ce8b24f527dd0592
Adi Mordo <adids1221@gmail.com>
2021-07-23 18:44:07 +0300

    Setup logout functionality added to the auth action and reducer

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,7 +6,9 @@ import {
     USER_LOADED,
     AUTH_ERROR,
     LOGIN_SUCCESS,
-    LOGIN_FAIL
+    LOGIN_FAIL,
+    LOGOUT_FAIL,
+    LOGOUT
 } from "./types";
 import setAuthToken from '../utils/setAuthToken';
 
@@ -89,4 +91,9 @@ export const login = (email, password) => async dispatch => {
             type: LOGIN_FAIL
         });
     }
+};
+
+//Logout and clear profile
+export const logout = () => dispatch => {
+    dispatch({ type: LOGOUT });
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -38,20 +38,24 @@ router.post('/', [
         let user = await User.findOne({ email });
 
         if (!user) {
-            return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+            return res
+                .status(400)
+                .json({ errors: [{ msg: 'Invalid Credentials' }] });
         }
 
         const isMatch = await bcrypt.compare(password, user.password);
 
         if (!isMatch) {
-            return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+            return res
+                .status(400)
+                .json({ errors: [{ msg: 'Invalid Credentials' }] });
         }
 
         const payload = {
             user: {
                 id: user.id
             }
-        }
+        };
 
         jwt.sign(
             payload,
@@ -62,7 +66,6 @@ router.post('/', [
                 res.json({ token });
             }
         );
-
     } catch (error) {
         console.error(error.message);
         res.status(500).send('Server Error');
